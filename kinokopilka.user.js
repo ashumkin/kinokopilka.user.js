100d6f481d007b48a167a7a294d8b5d90c4ee0a0
Alexey Shumkin <Alex.Crezoff@gmail.com>
2013-12-14 12:59:51 +0400

    fixed: script does not work in Firefox
    Script works in Chromium but not in FF. Make it work in FF.
    Also, add metadata: author, license, etc.

--- kinokopilka.user.js
+++ kinokopilka.user.js
@@ -1,22 +1,35 @@
 // ==UserScript==
 // @name       Kinokopilka expand all torrents
+// @author     Alexey Shumkin
+// @license    MIT
 // @namespace  http://kinokopilka.tv
-// @version    0.1
+// @version    0.2
+// @history    0.2 - Add Opera/Firefox support. Added metadata headers.
+// @history    0.1 - initial version
 // @description  Expands all folded torrents blocks and enables last "Enqueue torrent" link
+// @include    http://www.kinokopilka.tv/movies/*
 // @match      http://www.kinokopilka.tv/movies/*
+// @grant      none
 // @copyright  2012+, Alexey Shumkin
 // ==/UserScript==
-function expand_all_hidden() {
-    var entries = this.document.getElementsByTagName("div");
-    for (var i = 0; i < entries.length; i++ ) {
-        // find post
-        var entry = entries[i];
-        if ((entry.getAttribute('class') == 'torrent-info' )
-             || (entry.getAttribute('class') == 'media')
-            || entry.id.match(/xbt_file_[0-9]+_bookmark_widget/))
-        {
-            entry.setAttribute('style', 'display: block');
+!function (win) {
+	if (win.self != win.top) return;
+
+    function expand_all_hidden() {
+        var entries = this.document.getElementsByTagName("div");
+        for (var i = 0; i < entries.length; i++ ) {
+            // find post
+            var entry = entries[i];
+            if ((entry.getAttribute('class') == 'torrent-info' )
+                 || (entry.getAttribute('class') == 'media')
+                || entry.id.match(/xbt_file_[0-9]+_bookmark_widget/))
+            {
+                entry.setAttribute('style', 'display: block');
+            }
         }
     }
-}
-expand_all_hidden();
+	win.addEventListener("load", function() {
+        expand_all_hidden();
+	}, false);
+
+}(typeof unsafeWindow == 'undefined' ? window : unsafeWindow)
